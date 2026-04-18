2ae23bd7baf9d7d18e58bd341369ad8346873a60
thavisouk <bee-t1@hotmail.com>
2026-04-18 15:38:01 +0700

    add service get product

--- src/main/java/com/lbb/customer/buygold/BuyGoldController.java
+++ src/main/java/com/lbb/customer/buygold/BuyGoldController.java
@@ -1,13 +1,10 @@
-package com.lbb.customer.statement;
+package com.lbb.customer.buygold;
 
 import com.lbb.customer.statement.model.StatementReq;
 import com.lbb.customer.statement.model.StatementRes;
-import com.lbb.customer.statement.model.listaccount.ListAccountReq;
-import com.lbb.customer.statement.model.listaccount.ListAccountRes;
 import com.lbb.customer.statement.service.StatementService;
 
 import com.lbb.customer.util.DecodeToken;
-import com.lbb.customer.util.DecodeTokenObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +14,9 @@ import org.springframework.security.core.Authentication;
 
 @RestController
 @RequestMapping("${baseurl}")
-public class StatementController {
+public class BuyGoldController {
 
-    private static final Logger logger = LogManager.getLogger(StatementController.class);
+    private static final Logger logger = LogManager.getLogger(BuyGoldController.class);
 
     @Autowired
     StatementService statementService;
@@ -44,22 +41,6 @@ public class StatementController {
 
         decodeToken.decodeToken(authentication);
         return statementService.getStatement(req);
-    }
-
-    @GetMapping(value = "/list-td-account",
-            produces = "application/json", // This tells the client we return JSON
-            headers = {
-                    "Accept-Encoding=identity" // Keep this if your logic strictly requires it
-            })
-
-    public ListAccountRes listTDAccount(
-            Authentication authentication,
-            @RequestBody ListAccountReq req
-    ) {
-        DecodeTokenObject decodeTokenObject = new DecodeTokenObject();
-        decodeTokenObject =  decodeToken.decodeToken(authentication);
-
-        return statementService.getTDAccount(decodeTokenObject ,req.getPhone() );
     }
 
 }
